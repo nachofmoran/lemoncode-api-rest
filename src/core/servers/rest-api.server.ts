3e836753611a603331b80f812cda9ff677ff919d
nachofmoran <nachofmoran@gmail.com>
2025-12-06 12:40:04 +0100

    fix: se quita comentario

--- src/core/servers/rest-api.server.ts
+++ src/core/servers/rest-api.server.ts
@@ -6,12 +6,5 @@ export const createRestApiServer = () => {
   const app = express();
   app.use(express.json());
 
-  // app.use(express.json());
-  // app.use(
-  //   cors({
-  //     methods: ENV.CORS_METHODS,
-  //     origin: ENV.CORS_ORIGIN,
-  //   })
-  // );
   return app;
 };
